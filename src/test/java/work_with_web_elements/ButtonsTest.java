464f5be6ff81da51c6207a240e4f38fa8846eb94
Vlad <vyuzkevych@gmail.com>
2024-03-08 18:33:32 +0200

    add tests

--- src/test/java/work_with_web_elements/ButtonsTest.java
+++ src/test/java/work_with_web_elements/ButtonsTest.java
@@ -3,11 +3,12 @@ package work_with_web_elements;
 import org.testng.annotations.Test;
 
 import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.byValue;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class RadioButtonTest {
+public class ButtonsTest {
 
     @Test
     public void verifyRadioBtn() {
@@ -16,5 +17,29 @@ public class RadioButtonTest {
         // selenide selectRadio() method works only if there is a 'value' attribute exist
         $(byValue("option2")).shouldNotBe(selected).selectRadio("option2");
         $(byValue("option2")).shouldBe(selected);
+    }
+
+    @Test
+    public void verifyDoubleClickOnBtn() {
+        open("https://demoqa.com/buttons");
+
+        $("#doubleClickBtn").doubleClick();
+        $("#doubleClickMessage").shouldHave(text("You have done a double click"));
+    }
+
+    @Test
+    public void verifyContextClickOnBth() {
+        open("https://demoqa.com/buttons");
+
+        $("#rightClickBtn").contextClick();
+        $("#rightClickMessage").shouldHave(text("You have done a right click"));
+    }
+
+    @Test
+    public void verifyClickOnBtn() {
+        open("https://demoqa.com/buttons");
+
+        $(byText("Click Me")).click();
+        $("#dynamicClickMessage").shouldHave(text("You have done a dynamic click"));
     }
 }
