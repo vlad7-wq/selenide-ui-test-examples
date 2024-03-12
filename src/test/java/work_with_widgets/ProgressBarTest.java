b8c74a9cd4480908fabf7101e0e6e302d17f0929
Vlad <vyuzkevych@gmail.com>
2024-03-12 09:26:58 +0200

    add progress bar test

---
+++ src/test/java/work_with_widgets/ProgressBarTest.java
@@ -0,0 +1,23 @@
+package work_with_widgets;
+
+import com.codeborne.selenide.Configuration;
+import org.testng.annotations.Test;
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class ProgressBarTest {
+
+    @Test
+    public void testProgressBar() {
+        Configuration.browserSize = "1920x1080";
+        open("https://demoqa.com/progress-bar");
+
+        $("#startStopButton").shouldHave(text("Start")).click();
+        actions().pause(Duration.ofSeconds(11)).perform();
+
+        $("#progressBar > div[class='progress-bar bg-success']").shouldHave(text("100%"));
+        $("#resetButton").shouldHave(text("Reset"));
+    }
+}
