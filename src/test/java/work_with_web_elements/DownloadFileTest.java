2b5e4c2633885362109b19580e7ba1fd0a1faa0d
Vlad <vyuzkevych@gmail.com>
2024-03-14 09:39:38 +0200

    add test for upload file

--- src/test/java/work_with_web_elements/DownloadFileTest.java
+++ src/test/java/work_with_web_elements/DownloadFileTest.java
@@ -3,6 +3,7 @@ package work_with_web_elements;
 import org.testng.annotations.Test;
 import java.io.File;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,8 +15,20 @@ public class DownloadFileTest {
     public void downloadFileTest() {
         open("https://the-internet.herokuapp.com/download");
 
-        File file = $(byText("SomeFile.txt")).download();
-        assertThat(file).hasName("SomeFile.txt");
-        assertThat(file).content().isEqualTo("This is some text");
+        File file = $(byText("some-file.txt")).download();
+
+        assertThat(file).hasName("some-file.txt");
+        assertThat(file).content().startsWith("blah");
+    }
+
+    @Test
+    public void uploadFileTest() {
+        open("https://the-internet.herokuapp.com/upload");
+
+        $("#file-upload").uploadFromClasspath("cuteCat.jpg");
+        $("#file-submit").click();
+
+        $("h3").shouldHave(text("File Uploaded!"));
+        $("#uploaded-files").shouldHave(text("cuteCat.jpg"));
     }
 }
